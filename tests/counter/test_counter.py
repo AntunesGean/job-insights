c730c120f455114c6f879082330b2e6c2854fa9d
AntunesGean <gean.antunes@gmail.com>
2023-01-31 11:13:11 -0300

    req 10 - ok, eu acho

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    result_python = count_ocurrences('data/jobs.csv', 'Python')
+    assert result_python == 1639
+
+    result_javascript = count_ocurrences('data/jobs.csv', 'javascript')
+    assert result_javascript == 122
