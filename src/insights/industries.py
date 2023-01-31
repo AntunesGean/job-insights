3581099a67ebe54e010600b6e0d4f311c7d391eb
AntunesGean <gean.antunes@gmail.com>
2023-01-31 10:39:07 -0300

    req 07 - ok

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -28,6 +28,9 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
+
+    jobs_list = [job for job in jobs if job['industry'] == industry]
+    return jobs_list
     """Filters a list of jobs by industry
 
     Parameters
